4a991f08c330445e9fdede0984b5fae9ed75ee4a
canhchimlac <canhchimlac.94@gmail.com>
2019-08-19 21:57:58 +0700

    add new

--- run.cpp
+++ run.cpp
@@ -1,6 +1,8 @@
-#include "TurnOverGame.h"
+//#include "TurnOverGame.h"
+#include "ClearingRobot.h"
 
 int main() {
-	main_TurnOverGame();
+	//main_TurnOverGame();
+	main_clearingRobot();
 	return 0;
 }
